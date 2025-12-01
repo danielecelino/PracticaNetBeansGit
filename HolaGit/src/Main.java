9553fc65419a72a7fa18038569347424f8a15f8e
danielecelino <danielecelino96@gmail.com>
2025-12-01 08:16:14 +0100

    Añadida clase Main con saludo personalizado

---
+++ HolaGit/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 6DAWM
+ * @version 1.0
+ * @see
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde NetBeans y Git! Soy [Daniele]" ); 
+    } 
+}
